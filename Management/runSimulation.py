cef3c48c2a552916a8d6137851bd11a611964eb0
Elias Lundheim <elias.lundheim@gmail.com>
2024-01-30 16:59:41 +0100

    ok starting to work with the git stuff, need to do ssh

--- Management/runSimulation.py
+++ Management/runSimulation.py
@@ -7,7 +7,6 @@ outPath = "/media/elias/dataStorage/output/"
 config = SimulationConfig(nx=15, ny=15, startLoad=0, 
                           loadIncrement=0.05, maxLoad=2)
 
-with SimulationManager(config, outPath, onTheCluster=False, 
-                            useProfiling=False) as manager:
-    manager.runSimulation()
-    manager.plot()
+manager = SimulationManager(config, outPath, useProfiling=False)
+manager.runSimulation()
+manager.plot()
